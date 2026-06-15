dd9384138ae63c0b61800587a3ce1113a29bab66
Shailendra0001 <shailendrachahan11223344@gmail.com>
2026-06-15 16:09:23 +0530

    my first c program with comments

---
+++ hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>  //  header file for input output
+int main(){
+    printf("hello world\n");
+    printf("My name is Shailendra Chauhan\n");
+    printf("my first github push\n");
+    return 0;  // program ends successsfuly
+}
