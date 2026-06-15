33e523033dba721ba6826ecd21bd670309f42163
Shailendra0001 <shailendrachahan11223344@gmail.com>
2026-06-15 16:36:09 +0530

    i learned about %s,%.2f and char array []

---
+++ variable.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main (){
+    char name []="Shailendra";
+    printf("my name is : %s\n",name);
+  int age = 17 ;
+  printf("my age is : %d\n",age);
+  float height = 5.9;
+  printf("my height is : %.2f\n",height);
+
+    return 0;
+}
