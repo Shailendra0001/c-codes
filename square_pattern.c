296d6cc0eec2c95d26e391786e14188aa202f6f2
Shailendra0001 <shailendrachahan11223344@gmail.com>
2026-06-17 11:09:15 +0530

    add triangle pattern

--- square_pattern.c
+++ square_pattern.c
@@ -4,9 +4,9 @@ int main (){
     printf("enter number: ");
     scanf("%d", &n);
     for (int i = 1; i<=n; i++){
-        for (int j =1; j<=n; j++){
+        for (int j =1; j<=i; j++){
 
-                printf("*");
+                printf("* ");
         }printf("\n");
     }
     return 0; 
