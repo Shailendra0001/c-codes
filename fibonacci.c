a875a17ce891750cd5e7e025427ba0ce92c775d4
Shailendra0001 <shailendrachahan11223344@gmail.com>
2026-06-23 11:33:55 +0530

    add a program fibonacci using recursion

---
+++ fibonacci.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int fibonacci(int n);
+
+int main(){
+    printf("%d", fibonacci(8));
+    return 0;
+}
+
+int fibonacci(int n){
+    if(n==0) return 0;
+    if(n==1) return 1;
+    return fibonacci(n-1) + fibonacci(n-2);
+}
