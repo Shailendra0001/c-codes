f4cc1f7ceb7c5fd5d79ea2e350eec1833e23a2bd
Shailendra0001 <shailendrachauhan11223344@gmail.com>
2026-07-10 20:38:33 +0530

    add a matric

---
+++ matric.c
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows, cols;
+
+    cout << "Enter number of rows: ";
+    cin >> rows;
+
+    cout << "Enter number of columns: ";
+    cin >> cols;
+
+    int arr[10][10];
+
+    cout << "Enter the elements:" << endl;
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "\nMatrix is:\n";
+
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
