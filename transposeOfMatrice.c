f496871b2eb90017baa7f1f8730c52932c3f0fb7
Shailendra Chauhan <shailendrachauhan11223344@gmail.com>
2026-08-19 21:00:25 +0530

    sum of three numbers

---
+++ transposeOfMatrice.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main(){
+    int arr[3][3] = {{1,2,3},{4,5,6},{7,8,9}};
+*/ 1 2 3 
+   4 5 6
+   7 8 9 */
+
+   int brr[3][3];
+  
+   for(int i=0; i<3; i++){
+    for(int j =0; j<3; j++){
+        arr[i][j]= brr[j][i];
+    }
+   }
+   for(int i=0; i<3; i++){
+    for (int j=0; j<  3; j++){
+        printf("%d ", brr[i][j]);
+    }
+   }
+    return 0;
+}
