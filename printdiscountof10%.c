d0170566655c185cc9f32af455dd052655332f55
Shailendra Chauhan <shailendrachauhan11223344@gmail.com>
2026-08-27 21:19:11 +0530

    print final price

--- printdiscountof10%.c
+++ printdiscountof10%.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
 int main(){
 
-    int cost, quantity, bill ;
-
-    printf("enter cost : ");
+    int cost, quantity, bill, final_bill;
+    printf("enter cost :");
     scanf("%d",&cost);
 
     printf("enter quantity : ");
     scanf("%d",&quantity);
 
-    bill= cost*quantity;
-    float final_bill = bill - (bill*0.10);
-    printf("final bill after discount is %f",final_bill);
+    bill = cost*quantity;
+    final_bill= bill - (bill*0.10);
+    printf("final bill is %d",final_bill);
     return 0;
 }
