7ee53830f9b3a4b074f29c0c405ba81c7cd39dd8
Shailendra Chauhan <shailendrachauhan11223344@gmail.com>
2026-09-03 21:24:07 +0530

    print roll number

--- printareaofrect.c
+++ printareaofrect.c
@@ -3,7 +3,7 @@ int main(){
 
     int l, b;
     printf("enter length and breadth :");
-    scanf("%d%d",&l,&b);
+    scanf("%d%d",&l,  &b);
     printf("area of rectangle is %d",l*b);
     return 0;
 }
---
+++ printrollno.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+
+    int rollno,
+    printf("enter roll no. :");
+    scanf("%d",&rollno);
+    ptintf("roll no. is : %d",rollno);
+    return 0;
+}
