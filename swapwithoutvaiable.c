38ece0ceb057a6d35dc8f47f7c7ce70b9da4c1a5
Shailendra Chauhan <shailendrachauhan11223344@gmail.com>
2026-09-11 20:15:24 +0530

    swap number without third variable

--- swapwithoutvaiable.c
+++ swapwithoutvaiable.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 int main(){
-    int a, b, c;
+
+    int a, b;
     printf("enter a and b :");
     scanf("%d%d",&a,&b);
-    c=a;
-    a=b;
-    b=c;
-    printf("a = %d, b = %d",a,b); b
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    printf("a = %d, b = %d",a,b);
     return 0;
 }
