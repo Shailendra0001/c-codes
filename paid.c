d28ef1192d9122c1ffa3e698be3d27fb7d2aa98a
Shailendra Chauhan <shailendrachauhan11223344@gmail.com>
2026-09-12 20:11:57 +0530

    print if bill is paid

---
+++ paid.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int bill;
+    printf("enter 1 for paid and 0 for not paid : ");
+    scanf("%d",&bill);
+    if(bill == 1){
+        printf("bill is paid");
+    }else {
+        printf("not paid");
+    }
+    return 0;
+}
