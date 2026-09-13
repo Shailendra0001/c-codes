9807ab5fd885f7e5618fc3fbb0dcdf2510fa67cf
Shailendra Chauhan <shailendrachauhan11223344@gmail.com>
2026-09-13 20:37:19 +0530

    print pass or fail

--- paid.c
+++ paid.c
@@ -3,7 +3,7 @@ int main(){
     int bill;
     printf("enter 1 for paid and 0 for not paid : ");
     scanf("%d",&bill);
-    if(bill == 1){
+    if(bill == 1){ 
         printf("bill is paid");
     }else {
         printf("not paid");
